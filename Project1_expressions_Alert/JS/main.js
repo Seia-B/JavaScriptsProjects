052bd53afb80eb690c91cea6462631cee4ddfded
Patricia <P.Badeau@outlook.com>
2025-11-11 11:14:41 -0700

    First JavaScript Assignement for The Academy of Learning Career College.

---
+++ Project1_expressions_Alert/JS/main.js
@@ -0,0 +1,33 @@
+   document.write('This is a basic JavaScript Alert.<br>')// This is a assignment to perform an Alert with JavaScript
+
+   var x = "This is a second message made with JavaScript"//This is to assign a variable and display it on the screen
+
+   document.write(x);//Displaying the assigned variable on the screen
+
+   let message = "This is a third message with JavaScript"// Use of the let
+
+   window.alert(message)// displays the Let on the screen
+
+   document.write('<br>This an exemple of \"Quotation mark\"<br> and \'Aspotrophe\'.')//This is an example for displaying Quotations and Apostrophe.
+
+   document.write("<br>\"Perfection is not attainable,"
+    + " but if we chase perfection we can catch excellence,\" " 
+    + "-by Vince Lombardi."); // this is a concatenation
+
+    var B = "<br>\"Logic will get you from A to B, "
+    + " Imagination will take you everywhere.\" "
+    + " -Albert Einstein." + "<br>This is one of my favorite quotes." // this is a concatenated string
+
+    document.write(B);
+
+    var MainCharacter = "<br>Harry Potter", Friend1= "Ron Weasley", Friend2= "Herminone Granger",
+    MainAntagonist = "Lord Voldemort", Antogonist1= "Lucius Malfoy", Antagonist2 = "Bellatrix Lestrange"; // This is a multiple variables assignment with only one variable shown on the screen
+
+    document.write(MainCharacter);// The chosen variable shown on the screen
+
+    document.write(63+25);//This is an expression assignment
+
+    var I = "<br>This is a beautiful world,"
+    var J = "when you realise you are but a spec of sand."
+
+    document.write(I+J); // concatenate two variables together.
